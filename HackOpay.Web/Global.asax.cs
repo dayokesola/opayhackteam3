9df6a3bdc0aa2f709285ee7de378c5775c53ee10
Dayo Okesola <dayokesolagmail.com>
2018-11-10 15:54:08 +0100

    u[dated

--- HackOpay.Web/Global.asax.cs
+++ HackOpay.Web/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HackOpay.Core.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,6 +19,8 @@ namespace HackOpay.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+           SqlitePoco.Setup();
         }
     }
 }
